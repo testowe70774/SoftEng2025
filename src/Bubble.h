540d1ff3f7d3c97160bef3621c826faafe1ee372
testowe70774 <katarzyna_zajac@icloud.com>
2025-05-08 20:13:30 +0200

    Dodano implementację Bubble Sort w pliku Bubble.h

--- src/Bubble.h
+++ src/Bubble.h
@@ -1,7 +1,6 @@
 #ifndef _BUBBLE_H
 #define _BUBBLE_H
 
-
 #include "SortTech.h"
 #include "SortError.h"
 #include "SortData.h"
@@ -12,9 +11,37 @@ class Bubble : public SortTech<T, Container> {
     inline SortError sort(SortData<T> & data) override;
 
 };
+
 template<class T, template<typename...> class Container>
 inline SortError Bubble<T, Container>::sort(SortData<T> & data) {
-  return SE_SUCCESS;
+
+  SortError error = SE_SUCCESS;
+
+  if (data.begin(error) == data.end(error)) {
+    return SE_SUCCESS; // Empty container, nothing to sort
+  }
+
+  if (data.size() <= 0) {
+    return SE_SUCCESS;
+  }
+
+  size_t n = static_cast<size_t>(data.size());
+  bool swapped;
+
+  for (size_t i = 0; i < n - 1; ++i) {
+    swapped = false;
+    for (size_t j = 0; j < n - i - 1; ++j) {
+      if (data[j] > data[j + 1]) {
+        data.swap(j, j + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+
+  return error;
 }
 
 #endif
