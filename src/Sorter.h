b4668589a47701fffa0e2147abc47e46b615f261
Marcin Hajder <marcin.hajder@gmail.com>
2025-05-09 22:51:12 +0200

    Added Sorter and SortData classes implementation

--- src/Sorter.h
+++ src/Sorter.h
@@ -5,8 +5,10 @@
 #include "SortError.h"
 #include "SortTechFactory.h"
 #include <mutex>
-using namespace std;
 #include <list>
+#include <future>
+#include <thread>
+
 using namespace std;
 
 template<class T, template<typename...> class Container> class SortData;
@@ -28,7 +30,7 @@ class SortDataBase;
 //This class serves as the main access point to the library's interface. All sorting requests should be processed in a multithreaded manner to ensure efficiency and performance.
 class CLIENT_API Sorter {
   public:
-    template<class T, template<typename...> class Container>
+    template<class T, template<typename...> class Container, SortTechType TT = STT_NONE>
     inline SortError sort(SortData<T, Container> & data);
 
 
@@ -38,8 +40,29 @@ class CLIENT_API Sorter {
     list<shared_ptr<SortDataBase>> m_queue;
 
 };
-template<class T, template<typename...> class Container>
+template<class T, template<typename...> class Container, SortTechType TT>
 inline SortError Sorter::sort(SortData<T, Container> & data) {
+
+  SortError error = SE_SUCCESS;
+
+  data.setState(OS_RUNNING);
+
+  auto tech = SortTechFactory::createSorter<T, Container, TT>(error);
+
+  if (error != SE_SUCCESS)
+    return error;
+
+  auto sort_thread = [&]()
+  {
+    tech->sort(data);
+
+    data.setState(OS_DONE);
+
+    data.notifyDone();
+  };
+
+  std::async(std::launch::async, sort_thread);
+
   return SE_SUCCESS;
 }
 
