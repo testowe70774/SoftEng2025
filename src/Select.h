c32f6ab708496ba6cb31dd5b7c854df813e45893
AleksanderWieland <aleks.w2002@gmail.com>
2025-05-20 00:44:40 +0200

    clang formatted

--- src/Select.h
+++ src/Select.h
@@ -1,18 +1,17 @@
 #ifndef _SELECT_H
 #define _SELECT_H
 
-#include "SortTech.h"
-#include "SortError.h"
 #include "SortData.h"
+#include "SortError.h"
+#include "SortTech.h"
 
-template<class T, template<typename...> class Container>
+template <class T, template <typename...> class Container>
 class Select : public SortTech<T, Container> {
-  public:
-    inline SortError sort(SortData<T> & data) override;
-
+public:
+  inline SortError sort(SortData<T> &data) override;
 };
-template<class T, template<typename...> class Container>
-inline SortError Select<T, Container>::sort(SortData<T> & data) {
+template <class T, template <typename...> class Container>
+inline SortError Select<T, Container>::sort(SortData<T> &data) {
 
   size_t n = data.size();
 
