86e79b3cbbe2ee24d1c8583fc707955a26b76a1d
AleksanderWieland <aleks.w2002@gmail.com>
2025-05-19 23:32:20 +0200

    fixed selection sort

--- src/Select.h
+++ src/Select.h
@@ -7,28 +7,28 @@
 
 template<class T, template<typename...> class Container>
 class Select : public SortTech<T, Container> {
-public:
-    inline SortError sort(SortData<T, Container > & data) override;
+  public:
+    inline SortError sort(SortData<T> & data) override;
 
 };
 template<class T, template<typename...> class Container>
-inline SortError Select<T, Container>::sort(SortData<T, Container> & data) {
+inline SortError Select<T, Container>::sort(SortData<T> & data) {
 
-    size_t n = data.size();
+  size_t n = data.size();
 
-    for (size_t i = 0; i < n; ++i) {
-        size_t min_idx = i;
-        for (size_t j = i + 1; j < n; ++j) {
-            if (data[j] < data[min_idx]) {
-                min_idx = j;
-            }
-        }
-        if (min_idx != i) {
-            data.swap(i, min_idx);
-        }
+  for (size_t i = 0; i < n; ++i) {
+    size_t min_idx = i;
+    for (size_t j = i + 1; j < n; ++j) {
+      if (data[j] < data[min_idx]) {
+        min_idx = j;
+      }
     }
+    if (min_idx != i) {
+      data.swap(i, min_idx);
+    }
+  }
 
-    return SE_SUCCESS;
+  return SE_SUCCESS;
 }
 
 #endif
