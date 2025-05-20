b3844ca61a1d3814912fa3f4c8a51c1c1803eb08
AleksanderWieland <aleks.w2002@gmail.com>
2025-05-20 09:54:12 +0200

    clang formatted, with Visual Studio

--- src/Select.h
+++ src/Select.h
@@ -8,26 +8,33 @@
 template <class T, template <typename...> class Container>
 class Select : public SortTech<T, Container> {
 public:
-  inline SortError sort(SortData<T> &data) override;
+    inline SortError sort(SortData<T> &data) override;
 };
 template <class T, template <typename...> class Container>
-inline SortError Select<T, Container>::sort(SortData<T> &data) {
+inline SortError Select<T, Container>::sort(SortData<T> &data)
+{
 
-  size_t n = data.size();
+    size_t n = data.size();
 
-  for (size_t i = 0; i < n; ++i) {
-    size_t min_idx = i;
-    for (size_t j = i + 1; j < n; ++j) {
-      if (data[j] < data[min_idx]) {
-        min_idx = j;
-      }
+    for (size_t i = 0; i < n; ++i)
+    {
+        size_t min_idx = i;
+        for (size_t j = i + 1; j < n; ++j)
+        {
+            if (data[j] < data[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i)
+        {
+            data.swap(i, min_idx);
+        }
     }
-    if (min_idx != i) {
-      data.swap(i, min_idx);
-    }
-  }
 
-  return SE_SUCCESS;
+
+    return SE_SUCCESS;
 }
+
 
 #endif
