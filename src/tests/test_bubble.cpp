6103fcc25bb226953c0834e924d61af819611609
testowe70774 <katarzyna_zajac@icloud.com>
2025-05-28 18:19:53 +0200

    Update test_bubble.cpp: Implement bubble sort test

--- src/tests/test_bubble.cpp
+++ src/tests/test_bubble.cpp
@@ -12,13 +12,23 @@ TEST(test_bubble, AddFunction)
 #else
     SortData<float, std::vector> data;
 #endif
-    int dsize = 128 + rand() % 100;
-    for (unsigned i = 0; i < dsize; i++)
-      data.add(float(rand() % 1024) / 4096);
+    std::vector<float> unsorted_data = {5.3, 2.1, 9.7, 1.4, 5.0, 6.2};
+    for (auto num : unsorted_data)
+        data.add(num);
 
     auto error = sorter->sort<float, std::vector, STT_BUBBLE>(data);
 
     ASSERT_EQ(error, SE_SUCCESS);
+
+    bool is_sorted = true;
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i - 1] > data[i]) {
+            is_sorted = false;
+            break;
+        }
+    }
+
+    ASSERT_TRUE(is_sorted) << "Bubble Sort NIE dziala poprawnie!";
 }
 
 
