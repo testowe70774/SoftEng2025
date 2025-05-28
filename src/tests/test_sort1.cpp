1cc1d7fdd82d8eba2f86c3438338b2b2c8e3345f
Michał Długołęcki <127815@student.uksw.edu.pl>
2025-05-29 00:38:49 +0200

    Implement test_sort1 with default technique for int, float, double

--- src/tests/test_sort1.cpp
+++ src/tests/test_sort1.cpp
@@ -1,74 +1,78 @@
 #include <gtest/gtest.h>
-#include <vector> // potrzebne, bo używamy std::vector
-#include "Sorter.h"
-#include "SortData.h" // potrzebne, bo sorter.sort(...) przyjmuje SortData<...>&
+#include <vector> // std::vector
+#include <algorithm> // std::is_sorted
+#include "Sorter.h" // Sorter, SortError
+#include "SortData.h" // SortData<>
 
+//
+// 1) Sprawdzenie, że można skonstruować i zniszczyć Sorter
+//
 TEST(test_sort1, AddFunction)
 {
     Sorter* sorter = new Sorter();
-    ASSERT_NE(sorter, nullptr) << "Na stercie powinien powstać obiekt Sorter";
+    ASSERT_NE(sorter, nullptr);
 
     delete sorter;
     sorter = nullptr;
-    ASSERT_EQ(sorter, nullptr) << "Po usunięciu wskaźnik powinien być nullptr";
+
+    ASSERT_EQ(sorter, nullptr);
 }
 
-// --- Test 1: domyślna technika sortowania dla intów ---
+//
+// 2) Domyślna technika sortuje int-y
+//
 TEST(test_sort1, DefaultTechniqueSortsInt)
 {
-    // 1) przygotowujemy nieposortowany kontener intów
-    std::vector<int> v{ 5, 1, 4, 2, 3 };
+    std::vector<int> input{ 5, 3, 1, 4, 2 };
 
-    SortData<int, std::vector> data(v);
+    SortData<int, std::vector> data;
+    data.add(input.data(), static_cast<unsigned int>(input.size()));
+
     Sorter sorter;
-
-    // 2) wywołujemy sortowanie
     SortError err = sorter.sort(data);
-    ASSERT_EQ(err, SE_SUCCESS) << "sort() powinien zwrócić SE_SUCCESS";
+    ASSERT_EQ(err, SE_SUCCESS);
 
-    // 3) sprawdzamy, czy wynik jest posortowany
-    const auto& sorted = data.getData();
-    for (size_t i = 1; i < sorted.size(); ++i)
-    {
-        ASSERT_LE(sorted[i - 1], sorted[i])
-            << "Wektor intów nie jest posortowany na pozycji " << i;
-    }
+    const auto& result = data.data();
+    ASSERT_TRUE(std::is_sorted(result.begin(), result.end()))
+        << "Int vector nie jest posortowany!";
 }
 
-// --- Test 2: domyślna technika sortowania dla floatów ---
+//
+// 3) Domyślna technika sortuje float-y
+//
 TEST(test_sort1, DefaultTechniqueSortsFloat)
 {
-    std::vector<float> v{ 3.14f, 2.71f, 0.0f, -1.0f, 1.23f };
-    SortData<float, std::vector> data(v);
+    std::vector<float> input{ 3.14f, -1.0f, 0.0f, 2.71f, 1.23f };
+
+    SortData<float, std::vector> data;
+    data.add(input.data(), static_cast<unsigned int>(input.size()));
+
     Sorter sorter;
-
     SortError err = sorter.sort(data);
-    ASSERT_EQ(err, SE_SUCCESS) << "sort() powinien zwrócić SE_SUCCESS";
+    ASSERT_EQ(err, SE_SUCCESS);
 
-    const auto& sorted = data.getData();
-    for (size_t i = 1; i < sorted.size(); ++i)
-    {
-        ASSERT_LE(sorted[i - 1], sorted[i])
-            << "Wektor floatów nie jest posortowany na pozycji " << i;
-    }
+    const auto& result = data.data();
+    ASSERT_TRUE(std::is_sorted(result.begin(), result.end()))
+        << "Float vector nie jest posortowany!";
 }
 
-// --- Test 3: domyślna technika sortowania dla double ---
+//
+// 4) Domyślna technika sortuje double
+//
 TEST(test_sort1, DefaultTechniqueSortsDouble)
 {
-    std::vector<double> v{ 9.99, 3.33, 7.77, 1.11, 5.55 };
-    SortData<double, std::vector> data(v);
+    std::vector<double> input{ 9.99, 3.33, 7.77, 1.11, 5.55 };
+
+    SortData<double, std::vector> data;
+    data.add(input.data(), static_cast<unsigned int>(input.size()));
+
     Sorter sorter;
-
     SortError err = sorter.sort(data);
-    ASSERT_EQ(err, SE_SUCCESS) << "sort() powinien zwrócić SE_SUCCESS";
+    ASSERT_EQ(err, SE_SUCCESS);
 
-    const auto& sorted = data.getData();
-    for (size_t i = 1; i < sorted.size(); ++i)
-    {
-        ASSERT_LE(sorted[i - 1], sorted[i])
-            << "Wektor double nie jest posortowany na pozycji " << i;
-    }
+    const auto& result = data.data();
+    ASSERT_TRUE(std::is_sorted(result.begin(), result.end()))
+        << "Double vector nie jest posortowany!";
 }
 
 int main(int argc, char** argv)
